8dfca8dd12a873f02b7394d46a7804a770eb6cb6
kumar shaswat <Kumarsha9090@gmail.com>
2025-03-20 09:30:51 +0530

    Updates

---
+++ precache-manifest.26fd1d3c8bd71b33609e519aac13214a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33c3e819a730244c0ae472885bce357",
+    "url": "/chicken_kurtis/index.html"
+  },
+  {
+    "revision": "06ef8a3234489d5d4891",
+    "url": "/chicken_kurtis/static/css/main.2590b1b5.chunk.css"
+  },
+  {
+    "revision": "bcfb9bf52412f7de15d3",
+    "url": "/chicken_kurtis/static/js/2.1e643600.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/chicken_kurtis/static/js/2.1e643600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bed1d53ea03d594c2e59",
+    "url": "/chicken_kurtis/static/js/3.79551a88.chunk.js"
+  },
+  {
+    "revision": "06ef8a3234489d5d4891",
+    "url": "/chicken_kurtis/static/js/main.d53b6388.chunk.js"
+  },
+  {
+    "revision": "0da912e324b2665d71c7",
+    "url": "/chicken_kurtis/static/js/runtime-main.7ed52c80.js"
+  },
+  {
+    "revision": "2a37940ef292ae71bf00dfbbb47cbae2",
+    "url": "/chicken_kurtis/static/media/item11_463_fabric_cotton.2a37940e.jpg"
+  }
+]);
